cf0ba4f8672cfabfa1aefdde1ace318ce30d89fd
Tyler Condit <tyreid27@users.noreply.github.com>
2022-09-04 11:38:43 +0800

    updates to red agent

--- RedAgent.java
+++ RedAgent.java
@@ -4,17 +4,32 @@ import java.util.*;
  */
 
 public class RedAgent {
-    int messagePotency;
 
     public RedAgent() {
-        this.messagePotency = 1;
+
     }
 
-    public void redTurn(GreenAgent[] greenTeam) {
+    public double uncertaintyChange(int messagePotency) {
+        double uncertaintyChange
         Random rand = new Random();
-        messagePotency = rand.nextInt((5-1) + 1) + 1;
+        uncertainityChange = messagePotency-1 + (rand * (messagePotency - (messagePotency-1)));
+        return uncertainityChange;
+    }
+
+    public GreenAgent[] redTurn(GreenAgent[] greenTeam) {
+        Random rand = new Random();
+        GreenAgent[] newGreenTeam = greenTeam;
+        int messagePotency = rand.nextInt((5-1) + 1) + 1;
         for (int i = 0; i < greenTeam.length; i++) {
+            double uncertaintyChange;
             // loop through greenteam members and interact with them.
+            if (greenTeam[i].willVote) {
+
+            } else {
+                uncertaintyChange = calculateUncertainty(messagePotency);
+                
+            }
         }
+        return newGreenTeam;
     }
 }
