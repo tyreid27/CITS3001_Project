a3cdc3c6e483f246da5dff124586347148975121
Alan-K-Wan <97392979+Alan-K-Wan@users.noreply.github.com>
2022-09-04 14:54:30 +0800

    added file input to initialise the agents

--- Game.java
+++ Game.java
@@ -1,45 +1,49 @@
 import java.util.*;
-
+import java.io.*;  
 /**
  * This is the class to call to start the game
  * Compile with "javac Game.java GreenAgent.java RedAgent.java GreyAgent.java"
  * Run with "java Game"
  */
 public class Game{
-    // number of days (rounds) until the game stops
+    // number of days in the election campaign
     int daysToElection;
     // the day into the election campaign
     int day;
-    // total number of agents in the game in total (green + grey)
+    // total number of agents in the game in total
     int numAgents;
     // number of green agents
     int nGreen;
     // number of grey agents
     int nGrey;
-    // number of agents that will (currently) be voting at the election
+    // number of agents that are currently going to vote at the election
     int willVote;
-    // array of our green team agents
-    GreenAgent[] greenTeam;
-    // the red agent
+    // our agents
+    ArrayList<GreyAgent> greyTeam;
+    ArrayList<GreenAgent> greenTeam;
     RedAgent redAgent;
+    BlueAgent blueAgent;
 
-    public Game(int days, int agents){
+    public Game(int days){
         this.daysToElection = days;
-        this.numAgents = agents;
-        // get the number of green agents
-        this.nGreen = (int) Math.floor(agents * 0.9);
         // initialise array of green team members 
-        this.greenTeam = new GreenAgent[nGreen];
-        for(int i = 0; i < nGreen; i++){
-            greenTeam[i] = new GreenAgent();
-        }
+        this.greenTeam = new ArrayList<GreenAgent>();
         this.redAgent = new RedAgent();
+        this.blueAgent = new BlueAgent();
     }
 
     public void nextRound(){
         day++;
         System.out.println("day " + day);
         //redAgent.redTurn(greenTeam);
+    }
+
+    public void addAgent(String info){
+        String[] columns = info.split(",");
+    }
+
+    public void newConnection(int id1, int id2){
+        // stuff here
     }
 
     public void start(){
@@ -50,9 +54,22 @@ public class Game{
     }
     
     public static void main(String[] args){
-        //open a file here and input the params into the game
-
-        Game game = new Game(5, 10);
+        // create a new game
+        Game game = new Game(5);
+        // initialise all the agents
+        try{
+            File agents = new File("agents.csv");
+            Scanner agent = new Scanner(agents);
+            while (agent.hasNextLine()){
+                String data = agent.nextLine();
+                game.addAgent(data);
+            }
+            agent.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("An error occured while trying to open 'agents.csv");
+        }
+        // start the game
         game.start();
     }
 }
