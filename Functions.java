255947cd7661653a34ed4324f7c43ba3085e6a75
Tyler Condit <tyreid27@users.noreply.github.com>
2022-09-18 19:25:10 +0800

    created new functions class to hold functions for use across different files

---
+++ Functions.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Functions {
+    public static int changeDirectionProbabilty(int uncertainty) {
+        int probability = 0;
+        if (uncertainty >= 0 && uncertainty <= 1) {
+            probability = 95;
+        }
+        if (uncertainty > 1 && uncertainty <= 2) {
+            probability = 90;
+        }
+        if (uncertainty > 2 && uncertainty <= 3) {
+            probability = 85;
+        }
+        if (uncertainty > 3 && uncertainty <= 4) {
+            probability = 80;
+        }
+        if (uncertainty > 4 && uncertainty <= 5) {
+            probability = 75;
+        }
+        if (uncertainty > 5 && uncertainty <= 6) {
+            probability = 70;
+        }
+        if (uncertainty > 6 && uncertainty <= 7) {
+            probability = 65;
+        }
+        if (uncertainty > 7 && uncertainty <= 8) {
+            probability = 60;
+        }
+        if (uncertainty > 8 && uncertainty <= 9) {
+            probability = 55;
+        }
+        if (uncertainty > 9 && uncertainty <= 10) {
+            probability = 50;
+        }
+        return probability;
+    }
+}
